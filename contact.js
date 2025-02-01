395b1e0d884bfde790d63c134fc8a6072caa7603
SimonCaldwell15 <W0468071@nscc.ca>
2025-02-01 14:26:44 -0500

    Updated contact form and styles

---
+++ contact.js
@@ -0,0 +1,23 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents default form submission
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert('Your message has been sent!');
+            form.reset();
+        } else {
+            alert('Oops! There was a problem submitting your form');
+        }
+    }).catch(error => {
+        alert('Oops! There was a problem submitting your form');
+    });
+});
